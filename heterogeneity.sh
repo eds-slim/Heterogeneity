5ca41664e4f819cf16bd6e9e08d2397915f8abbb
felenae <80704328+felenae@users.noreply.github.com>
2021-05-14 13:31:50 +0200

    Update heterogeneity.sh

--- heterogeneity.sh
+++ heterogeneity.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mask=lesion_dwi_space_thr_900_bin.nii.gz
 intensity=p03_1_r_FW.nii.gz
 
-matlab -nosplash -nodesktop -nojvm -r "cd('$DIR'); heterogeneity('$mask', '$intensity')"
+matlab -nosplash -nodesktop -nojvm -r "cd('$DIR'); heterogeneity('$mask', '$intensity'); quit"
